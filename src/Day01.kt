3c3170070a1fa671e45aaf655a97c9fae3580380
cak <cak@fastmail.com>
2021-12-04 03:13:32 -0500

    reupload day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = input.mapNotNull { it.toIntOrNull() }.filterIndexed { index, i ->
+        input.getOrNull(index - 1)?.toIntOrNull()?.let { i > it } ?: false
+    }.count()
+
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val intInput = input.mapNotNull { it.toIntOrNull() }
+        return intInput.filterIndexed { index, _ ->
+            if (index >= 3) {
+                intInput.slice(index - 3 until index).sum() < intInput.slice(index - 2 until index + 1).sum()
+            } else {
+                false
+            }
+        }.count()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
